ffdea65ed16e0762bb7a3edb23f69d85be8ff50d
Konrad Jachimstal <k.jachimstal@gmail.com>
2024-08-08 21:52:05 +0200

    Added CRUD for appointments

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@
 import app from '../utils/config'
 
 /* -------------------------------- Firebase -------------------------------- */
-import { get, getDatabase, ref } from 'firebase/database'
+import { get, getDatabase, push, ref, remove, set } from 'firebase/database'
 
 /* --------------------------- Fetch Appointments --------------------------- */
 export const fetchAppointments = async () => {
@@ -17,3 +17,24 @@ export const fetchAppointments = async () => {
   }
 }
 
+export const createAppointment = async (appointment) => {
+  const db = getDatabase(app)
+  const dbRef = push(ref(db, "appointments"))
+  set(dbRef, appointment)
+  .then(() => { console.log("Appointment has been added")})
+  .catch(() => console.log("Error while creating appointment"))
+}
+
+export const updateAppointment = async (appointment, appointment_id) => {
+  const db = getDatabase(app)
+  const dbRef = ref(db, `appointments/${appointment_id}`)
+  set(dbRef, appointment)
+  .then(() => { console.log("Appointment has been updated")})
+  .catch(() => console.log("Error while updating appointment"))
+}
+
+export const deleteAppointment = async (appointment_id) => {
+  const db = getDatabase(app)
+  const dbRef = ref(db, `appointments/${appointment_id}`)
+  await remove(dbRef)
+}
